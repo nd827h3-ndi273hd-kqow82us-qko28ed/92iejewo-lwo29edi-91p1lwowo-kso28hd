65abedf78e948469956c513ea7b14295081b7aad
nd827h3-ndi273hd-kqow82us-qko28ec <pickletalk11@gmail.com>
2026-05-07 12:18:25 +0800

    Update mm2.lua

--- games/mm2.lua
+++ games/mm2.lua
@@ -43,7 +43,7 @@ local knifeSpeedBuf  = {}
 local fbConn = nil
 local KNIFE_SPEED_CAP = 10
 local KNIFE_SPEED_DEF = 120
-local FAKE_BOMB_Y_OFFSET = 3.1
+local FAKE_BOMB_Y_OFFSET = 3.2
 local FAKE_BOMB_APEX_VEL = 3
 
 local function getLobbyPart()
